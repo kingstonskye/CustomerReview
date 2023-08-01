d5953f3446f05d4d1b7dd63df7c8c3d3816a014c
kingstonskye <125080702+kingstonskye@users.noreply.github.com>
2023-08-01 11:54:06 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+
+(function () {
+    const customerImage = document.getElementById('customer-image');
+    const customerName = document.getElementById('customer-name');
+    const customerText = document.getElementById('customer-text');
+
+
+const customers = [
+    {
+        name: 'Sarah',
+        image: 'img/0.jpg',
+        review: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi distinctio similique maxime veniam eos cumque nisi iure vel! Ad voluptas labore debitis! Eos aut nihil dolorum libero doloribus repudiandae alias?',
+    },
+
+    {
+        name:'Dave',
+        image: 'img/1.jpg',
+        review: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
+    },
+
+    {
+        name: 'SasukeFan69',
+        image: 'img/2.jpg',
+        review: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi distinctio similique maxime veniam eos cumque nisi iure vel! Ad voluptas labore debitis!',
+    },
+
+    {
+        name: 'John',
+        image: 'img/3.jpg',
+        review:'Lorem ipsum dolortlo.',
+    },
+
+    {
+        name: 'Arthur',
+        image: 'img/4.jpg',
+        review: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi distinctio similique.',
+    }
+];
+
+let index = 0;
+
+function updateCustomerInfo() {
+    customerImage.src = customers[currentIndex].image;
+    customerName.textContent = customers[currentIndex].name;
+    customerText.textContent = customers[currentIndex].review;
+}
+
+//function for prev button
+
+function showPreviousCustomer() {
+    index = (index - 1 + customers.length) % customers.length;
+    updateCustomerInfo();
+}
+
+function showNextCustomer() {
+    index = (index + 1 + customers.length) % customers.length;
+    updateCustomerInfo();
+}
+
+document.querySelector('.prevBtn').addEventListener('click', showPreviousCustomer);
+document.querySelector('.nextBtn').addEventListener('click', showNextCustomer);
+
+updateCustomerInfo();
+
+})();
